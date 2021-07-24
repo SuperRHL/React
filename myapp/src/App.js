5e15fb62345eb51bb5574240454d356a0efa57d1
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-07-24 21:31:20 +0530

    react improvements

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,7 @@
 import Ninjas from "./ninja";
 import React, {Component} from "react";
+import AddNinja from "./addNinja";
+
 class App extends Component {
   state = {
     ninjas: [
@@ -8,12 +10,22 @@ class App extends Component {
       { name: "Rowl", age: 20, belt: "Red", id: 3 },
     ],
   };
+  addNinja = (ninja)=> {
+    ninja.id = Math.random()
+    let ninjas = [...this.state.ninjas,ninja]
+    this.setState({ninjas: ninjas})
+  }
+  deleteNinja=(id)=>{
+    let ninjas = this.state
+    this.setState({})
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>Welcome!</p>
-        <Ninjas ninjas={this.state.ninjas} />
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
--- myapp/src/ninja.js
+++ myapp/src/ninja.js
@@ -1,19 +1,21 @@
-import React, { Component } from "react";
-
-function Ninjas(){
-  
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map((ninja) => {
+import React from "react";
+// class based components have states and functional components are more focused on the ui
+const Ninjas = ({ ninjas,deleteNinja }) => {
+  const ninjaList = ninjas.map((ninja) => {
+    if (ninja.age > 20) {
       return (
         <div className="ninja" key={ninja.id}>
           <div>Name: {ninja.name}</div>
           <div>Age: {ninja.age}</div>
           <div>Belt: {ninja.belt}</div>
+          <button onClick={()=>{deleteNinja(ninja.id)}}>Delete</button>
         </div>
       );
-    });
-    return <div className="ninja-list">{ninjaList}</div>;
-  }
-
+    } else {
+      return null;
+    }
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninjas;
