07ef71d7ba5334735d53e88f82c99a8c74080f57
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-07-25 18:08:48 +0530

    add todo function

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import AddTodo from "./AddTodo";
 import Todos from "./Todos";
 class App extends Component {
   state = {
@@ -11,6 +12,12 @@ class App extends Component {
     const todos = this.state.todos.filter(todo=>{return todo.id!==id})
     this.setState({todos: todos})
   };
+  addTodo =(todo)=>{
+    todo.id = Math.random()
+    let todos= [...this.state.todos, todo]
+    this.setState({todos: todos})
+
+  }
   render() {      
     return (
       <div className="todo-app container">
@@ -19,6 +26,7 @@ class App extends Component {
           todos={this.state.todos}
           deleteTodo={this.deleteTodo}
         ></Todos>
+        <AddTodo addTodo={this.addTodo}></AddTodo>
       </div>
     );
   }
