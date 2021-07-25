34cd52e34e978c2adc1069691331434ce08fd72e
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-07-25 18:48:22 +0530

    final commit

--- todoapp/src/AddTodo.js
+++ todoapp/src/AddTodo.js
@@ -1,21 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class AddTodo extends Component{
-    state={content: ''}
-    handleChange =(e)=>{this.setState({content: e.target.value})}
-    handleSubmit = (e)=>{
-        e.preventDefault();
-        this.props.addTodo(this.state)
-    }
-    render(){
-        return(
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Add new Todo: </label>
-                    <input type="text" onChange={this.handleChange}></input>
-                </form>
-            </div>
-        )
-    }
+class AddTodo extends Component {
+  state = { content: "" };
+  handleChange = (e) => {
+    this.setState({ content: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state);
+    this.setState({content:''})
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Add new Todo: </label>
+          <input type="text" onChange={this.handleChange} value={this.state.content}></input>
+        </form>
+      </div>
+    );
+  }
 }
-export default AddTodo
+export default AddTodo;
