0280de9f844581f7045b42d38a24854dcdbe0849
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-07-25 17:43:50 +0530

    todo app

---
+++ todoapp/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Todos from "./Todos";
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy milk" },
+      { id: 2, content: "play mario kart" },
+    ],
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo=>{return todo.id!==id})
+    this.setState({todos: todos})
+  };
+  render() {      
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todos</h1>
+        <Todos
+          todos={this.state.todos}
+          deleteTodo={this.deleteTodo}
+        ></Todos>
+      </div>
+    );
+  }
+}
+
+export default App;
