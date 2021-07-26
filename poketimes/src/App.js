d8599729c207be1c93d7026b11c66f17e59037d9
RHL <46913791+SuperRHL@users.noreply.github.com>
2021-07-26 17:24:57 +0530

    colourful about

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -4,13 +4,14 @@ import Navbar from './components/navbar';
 import {BrowserRouter, Route} from 'react-router-dom'
 import About from './components/About'
 import Contact from './components/Contact'
+//higher order components wrap and add extra functionality to existing components
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
       <div className="App">
         <Navbar></Navbar>
-        <Route path='/home' component={Home}/>
+        <Route exact path='/' component={Home}/>
         <Route path='/about' component={About}/>
         <Route path='/contact' component={Contact}/>
       </div>
--- poketimes/src/components/navbar.js
+++ poketimes/src/components/navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
-
+import { Link, NavLink } from "react-router-dom";
+//the only difference between link and navlink is that with navlink we have classname as active and can be used for further styling of elements
 const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
         <a className="brand-logo">Poke'Times</a>
         <ul className="right">
-            <li><a href="/home">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><Link to ="/">Home</Link></li>
+            <li><Link to ="/about">About</Link></li>
+            <li><Link to ="/contact">Contact</Link></li>
         </ul>
       </div>
     </nav>
